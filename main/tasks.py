f48ef587a3264b8ecd949be2da1da63263bfec0f
Denis Gryaznov <distaboo@mail.ru>
2019-06-02 12:35:52 +0600

    first commit

--- main/tasks.py
+++ main/tasks.py
@@ -1,2 +1,105 @@
 from background_task import background
+import time
+import re
+from main.models import *
+from InstagramAPI import InstagramAPI
+
+pars = Parsing.objects.get(pk = 1)
+usernames = Acc.objects.values_list('username')
+passwords = Acc.objects.values_list('password')
+curname = ''
+apis = []
+apiswork = []
+countEmail = 0
+countBioEmail = 0
+counter = 0
+def cnt(n):
+    for i in range(n):
+            global counter
+            counter = counter + 1
+            time.sleep(1)
+            print(counter)
+
+
+for u,p in zip(usernames, passwords):
+    apis.append(InstagramAPI(u[0],p[0]))
+
+    print('nhey')
+#api = InstagramAPI("willseerussianparadise", "xflava13")
+print(usernames)
+def apiLogin(api,user):
+    if (api.login()):
+        #api.getSelfUserFeed()  # get self user feed
+        #print(api.LastJson)  # print last response JSON
+        apiswork.append(api)
+        print("Login succes {}!".format(user[0]))
+    else:
+        print("Can't login {}!".format(user[0]))
+        #apis.remove(api)
+
+
+for a , u in zip(apis,usernames):
+    print(a)
+    print('nhey')
+    time.sleep(3)
+    #api = InstagramAPI(a[0],a[1])
+    apiLogin(a,u)
+
+@background(schedule=1)
+def parsing(id):
+    pars = Parsing.objects.get(pk=1)
+    pars.currentState = True
+    foll = apiswork[0].getTotalFollowings(id)
+    pars.currentTotal = len(foll)
+    pars.save()
+    i = 0
+    sum = []
+    sumBio = []
+    l = len(apiswork)
+    for k in foll:
+        if pars.currentState == True:
+            n = i % l
+            print(i)
+            i = i + 1
+            pars.currentVar = i
+            pars.save()
+            time.sleep(3/l/2)
+            try:
+                t = apiswork[n].getUsernameInfo(k['pk'])
+                lj = apiswork[n].LastJson['user']
+            except:
+                pass
+            global countBioEmail
+            global countEmail
+            try:
+                b = lj['biography']
+                e = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", b)
+                if not (len(e)==0):sumBio.append(e[0])
+                countBioEmail = len(sumBio)
+            except:
+                pass
+            try:
+                e = lj['public_email']
+                #print(lj)
+                if not (len(e)==0):sum.append(e)
+                #print(apiswork[n].LastJson['user']['public_email'])
+                countEmail = len(sum)
+            except:
+                pass
+        else:
+            break
+    pars.info = 'Парсинг {} завершен'.format(curname)
+    pars.currentState = False
+    pars.result = str(sum)
+    pars.save()
+
+    f = open('listButton.txt', 'w')
+    f.write("\n".join(sum))
+    f.close()
+
+    fb = open('list.txt', 'w')
+    fb.write("\n".join(sumBio))
+    print(sumBio)
+    fb.close()
+
 
--- main/views.py
+++ main/views.py
@@ -6,108 +6,21 @@ import requests
 import time
 from threading import Thread
 from django.shortcuts import redirect
-import re
+
 import os
 from django.conf import settings
 from django.http import HttpResponse, Http404
 
-pars = Parsing.objects.get(pk = 1)
-usernames = Acc.objects.values_list('username')
-passwords = Acc.objects.values_list('password')
+from main.tasks import parsing
+
+
 curname = ''
-apis = []
-apiswork = []
-countEmail = 0
-countBioEmail = 0
-counter = 0
-def cnt(n):
-    for i in range(n):
-            global counter
-            counter = counter + 1
-            time.sleep(1)
-            print(counter)
-
-thread = Thread(target=cnt)
-thread.daemon = True
-#thread.start()
-for u,p in zip(usernames, passwords):
-    apis.append(InstagramAPI(u[0],p[0]))
-
-    print('nhey')
-#api = InstagramAPI("willseerussianparadise", "xflava13")
-print(usernames)
-def apiLogin(api,user):
-    if (api.login()):
-        #api.getSelfUserFeed()  # get self user feed
-        #print(api.LastJson)  # print last response JSON
-        apiswork.append(api)
-        print("Login succes {}!".format(user[0]))
-    else:
-        print("Can't login {}!".format(user[0]))
-        #apis.remove(api)
+countEmail = ''
+countBioEmail = ''
 
 
-for a , u in zip(apis,usernames):
-    print(a)
-    print('nhey')
-    time.sleep(3)
-    #api = InstagramAPI(a[0],a[1])
-    apiLogin(a,u)
 
-
-def parsing(id):
-    pars.currentState = True
-    foll = apiswork[0].getTotalFollowings(id)
-    pars.currentTotal = len(foll)
-    pars.save()
-    i = 0
-    sum = []
-    sumBio = []
-    l = len(apiswork)
-    for k in foll:
-        if pars.currentState == True:
-            n = i % l
-            print(i)
-            i = i + 1
-            pars.currentVar = i
-            pars.save()
-            time.sleep(3/l/2)
-            t = apiswork[n].getUsernameInfo(k['pk'])
-            lj = apiswork[n].LastJson['user']
-            global countBioEmail
-            global countEmail
-            try:
-                b = lj['biography']
-                e = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", b)
-                if not (len(e)==0):sumBio.append(e[0])
-                countBioEmail = len(sumBio)
-            except:
-                pass
-            try:
-                e = lj['public_email']
-                #print(lj)
-                if not (len(e)==0):sum.append(e)
-                #print(apiswork[n].LastJson['user']['public_email'])
-                countEmail = len(sum)
-            except:
-                pass
-        else:
-            break
-    pars.info = 'Парсинг {} завершен'.format(curname)
-    pars.currentState = False
-    pars.result = str(sum)
-    pars.save()
-
-    f = open('listButton.txt', 'w')
-    f.write("\n".join(sum))
-    f.close()
-
-    fb = open('list.txt', 'w')
-    fb.write("\n".join(sumBio))
-    print(sumBio)
-    fb.close()
-
-
+pars = Parsing.objects.get(pk = 1)
 
 def download(request, path):
     file_path =  path
@@ -124,7 +37,7 @@ def getID(name):
     return r.json()['logging_page_id'][12:]
 
 def index(request):
-
+    pars = Parsing.objects.get(pk=1)
 
     context = {
         'total': pars.currentTotal,
@@ -140,26 +53,32 @@ def index(request):
 # Create your views here.
 def start(request):
     # if this is a POST request we need to process the form data
-
+    print('13')
     if pars.currentState == False:
+        print('13')
         if request.method == 'POST':
+            print('13')
             # create a form instance and populate it with data from the request:
             form = IdForm(request.POST)
             # check whether it's valid:
             if form.is_valid():
+                print('13')
                 global curname
                 global countEmail
                 global countBioEmail
+                print('13')
                 curname = request.POST['insid']
                 try:
                     id = getID(curname)
                 except:
                     pars.info = 'Некорректный логин: {}'.format(curname)
                     return redirect('/')
-                global thread
-                thread = Thread(target=parsing, args=(id,))
-                thread.daemon = True
-                thread.start()
+
+                #thread = Thread(target=parsing, args=(id,))
+                #thread.daemon = True
+                #thread.start()
+                print(id)
+                parsing(id)
                 # process the data in form.cleaned_data as required
                 # ...
                 # redirect to a new URL:
@@ -180,4 +99,5 @@ def stop(request):
     pars.info = 'Парсинг {} остановлен'.format(curname)
     pars.currentState = False
     pars.save()
+
     return redirect('/')
